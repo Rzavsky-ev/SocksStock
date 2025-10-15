9e1dc09ba96bccaada1c3ee7a8b85fce101dc4bf
Eduard.Rz <rzavsky.ev@gmail.com>
2025-10-15 20:12:10 +0300

    Первый коммит

---
+++ src/main/java/org/skypro/socksStock/SocksStockApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.socksStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/socksStock/SocksStockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.socksStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
